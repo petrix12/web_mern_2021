db3a22506e2a393f127b47311a7965a9a1ef2de4
Pedro Bazó <bazo.pedro@gmail.com>
2021-12-22 14:15:38 -0400

    Subiendo Cliente a Netlify

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import { HashRouter as Router, Route, Switch } from "react-router-dom"
 import routes from "./config/routes"
 import AuthProvider from "./providers/AuthProvider"
 import './App.scss';
